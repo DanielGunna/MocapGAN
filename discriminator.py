a97a95374c953f077695799611629ef390116d04
DanielGunna <danielgunna1408@gmail.com>
2018-03-23 19:02:02 -0300

    WIP build skeleton matrix

---
+++ discriminator.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 23 17:31:19 2018
+
+@author: 940437
+"""
+
+
+    
--- mocapgan.py
+++ mocapgan.py
@@ -1,5 +1,6 @@
 
 from skeleton import Skeleton
+import numpy
 
 class MocapGAN:
     
@@ -8,14 +9,46 @@ class MocapGAN:
 
     def read_file(self,file):
         file_skeleton = Skeleton(file,0.5)
-   
+        return file_skeleton;
     
+    def build_skeleton_matrix(self,skeleton):
+        joint_labels = []
+        self.get_children_joint_labels(joint_labels,skeleton.root)
+        skeleton_matrix = numpy.zeros((
+                len(joint_labels),
+                6,
+                skeleton.frames
+        ));
+       # for label,i in joint_labels,range(0,37):
+        #    print (label.name +" "+ i)
+                        
+            
+        
+        
+    def get_children_joint_labels(self,labels,joint):
+    
+        if joint.name == 'End effector':
+            labels.append('EndEffector' + joint.parent.name)
+        else:
+            labels.append(joint.name)
+        for child in joint.children:
+            self.get_children_joint_labels(labels,child)
+        
+    
+    def start(self):
+        self.build_skeleton_matrix(self.read_file("example.bvh"))
+        
+    
+
+    
+        
+   
          
 
 
 if __name__ == '__main__':
     mocap = MocapGAN();
-    mocap.read_file('example.bvh')
+    mocap.start()
 
     
 
